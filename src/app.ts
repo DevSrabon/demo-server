c01d46e66e60cae37a6676ffe4f3a7e9386cf6d8
devsrabon <et.srabon@gmail.com>
2024-04-16 08:20:04 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,6 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World docker!");
 });
 export default app;
